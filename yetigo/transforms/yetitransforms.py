8631a4f832228f63a2cc47631b07ce613b5a0dff
Sebdraven <sebdraven@protonmail.com>
2020-06-08 11:13:24 +0200

    add new file for analytics transforms

--- yetigo/transforms/yetitransforms.py
+++ yetigo/transforms/yetitransforms.py
@@ -189,40 +189,6 @@ class EntityToEntities(Transform):
             return response
 
 
-class VTHashYeti(Transform):
-    input_type = Hash
-    display_name = '[YT] Hash Virustotal'
-
-    def do_transform(self, request, response, config):
-        entity = request.entity
-        yeti = get_yeti_connection(config)
-
-        if yeti:
-            observable = yeti.observable_add(entity.value)
-            oneshot = yeti.get_analytic_oneshot('Virustotal')
-            res = yeti.analytics_oneshot_run(oneshot, observable)
-            if res:
-                virus_res = res['nodes'][0]
-                context_vt = list(
-                    filter(lambda x: x['source'] == 'virustotal_query',
-                           res['nodes'][0]['context']))
-                context_filter = sorted(context_vt,
-                                        key=lambda x: parser.parse(
-                                            x['scan_date']))
-                if len(context_filter) > 0:
-                    last_context = context_filter[0]
-                    vt_res = json.loads(last_context['raw'])
-                    for h in get_hash_entities(vt_res,
-                                               list_hash=['md5', 'sha256',
-                                                          'sha1']):
-                        if h.value != entity.value:
-                            response += h
-
-                    for ph in get_av_sig(vt_res['scans'].items()):
-                        response += ph
-            return response
-
-
 class AddDomain(Transform):
     input_type = Domain
     display_name = '[YT] Add Domain'
