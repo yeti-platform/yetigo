d08fe31061f5913020c7731bdf54dfa9ca0afa19
Sebdraven <sebdraven@protonmail.com>
2020-09-14 15:09:18 +0200

    add transform PT Reverse NS

--- yetigo/transforms/passivetotal.py
+++ yetigo/transforms/passivetotal.py
@@ -24,3 +24,17 @@ class PTPassiveDNSByIP(Transform):
         entity = request.entity
         res = run_oneshot('PassiveTotal Passive DNS', request, config)
         return do_pdns_pt(res, entity, response)
+
+
+class PTReverseNS(Transform):
+    input_type = Hostname
+    display_name = '[YT] PT Reverse NS'
+
+    def do_transform(self, request, response, config):
+        entity = request.entity
+        res = run_oneshot('PassiveTotal Passive DNS', request, config)
+        for item in res['nodes']:
+            entity_add = Ip(item['value'])
+            entity_add.link_label = 'Server NS'
+            response += entity_add
+        return response
